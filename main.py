6ef0e707b798ad9d9d21700f03232864ece83408
Kryptonite <1535820963@qq.com>
2022-11-14 22:36:46 +0800

    [mod] comments

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 import yaml
 
 with open('kyx.yaml') as f:
@@ -18,7 +17,8 @@ eval_words = eval_data['eval_words']
 driver = webdriver.Chrome()
 driver.get(eval_url)
 time.sleep(3)
-# send_keys设置input框内容  click处理点击
+
+# 登录，send_keys设置input框内容，click处理点击
 driver.find_element(By.XPATH, '//*[@id="user_name"]').send_keys(eval_data['username'])
 driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(eval_data['password'])
 driver.find_element(By.XPATH, '//*[@id="logon_button"]').click()
@@ -27,15 +27,16 @@ time.sleep(3)
 # 获取评估列表
 container = driver.find_element(By.ID, "myTaskContainer")
 
-# find the table
+# 找到评估列表
 table = container.find_element(By.TAG_NAME, "table")
-# find the second tbody
+# 去掉表头
 tbody = table.find_elements(By.TAG_NAME, "tbody")[1]
-# find all tr
+# 获取所有表项
 trs = tbody.find_elements(By.TAG_NAME, "tr")
 
 link_list = []
 
+# 获取所有评价链接
 for tr in trs:
     try:
         a = tr.find_element(By.TAG_NAME, "a")
@@ -44,7 +45,16 @@ for tr in trs:
     link = a.get_attribute("href")
     link_list.append(link)
 
-# 进入评估页面，这里先用日常反馈测试
+def evaluation():
+    """
+    评估有效认定：
+    - 单门课程作答时间不少于2分钟
+    - 填写开放性问题不少于10字
+    - 所有评分指标选项不完全一致
+    """
+    pass
+
+# 通过链接进入评估页面，这里先用日常反馈测试
 for link in link_list:
     driver.get(link)
     time.sleep(1)
